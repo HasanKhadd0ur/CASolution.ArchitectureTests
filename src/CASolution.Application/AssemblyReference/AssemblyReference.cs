75d887a1dad09fffab0dff3b9a509b06ee12ec22
hasan khaddour <hasan.b.khaddour@gmail.com>
2024-09-26 18:41:05 +0300

    Add projects for tests

---
+++ src/CASolution.Application/AssemblyReference/AssemblyReference.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace CASolution.Application.AssemblyReference
+{
+    public class AssemblyReference
+    {
+        public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
+
+    }
+}
---
+++ src/CASolution.Domain/AssemblyReference/AssemblyReference.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace CASolution.Domain.AssemblyReference
+{
+    public class AssemblyReference
+    {
+        public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
+
+    }
+}
