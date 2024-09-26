1398dbfada4ae4b18a76a2f2724b44d4815d3687
hasan khaddour <hasan.b.khaddour@gmail.com>
2024-09-26 18:44:15 +0300

    Add Assemblies Provider

---
+++ tests/CASolution.ArchitectureTests/Providers/IAssembliesProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CASolution.ArchitectureTests.Providers
+{
+    public interface IAssembliesProvider
+    {
+        public Assembly DomainAssembly { get; set; }
+        public Assembly ApplicationAssembly { get; set; }
+        public Assembly PresentationAssembly { get; set; }
+        public Assembly ContractsAssembly { get; set; }
+        public Assembly InfrastructureServicesAssembly { get; set; }
+        public Assembly InfrastructurePersistenceAssembly { get; set; }
+
+
+    }
+}
