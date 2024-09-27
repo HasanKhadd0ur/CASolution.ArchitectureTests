2a198437274b54e35c80808a890394fe7082b872
hasan khaddour <hasan.b.khaddour@gmail.com>
2024-09-27 19:22:56 +0300

    Add Naming Convention Helper

---
+++ tests/CASolution.ArchitectureTests/Helpers/NamingConventionHelper.cs
@@ -0,0 +1,25 @@
+﻿using NetArchTest.Rules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CASolution.ArchitectureTests.Helpers
+{
+    public class NamingConventionHelper
+    {
+        public static bool InterfaceImplementingTest(Assembly assembly ,Type type , string suffix)
+        {
+            var result = Types.InAssembly(assembly)
+                            .That()
+                            .ImplementInterface(type)
+                            .Should()
+                            .HaveNameEndingWith(suffix)
+                            .GetResult();
+
+            return result.IsSuccessful;
+        }
+    }
+}
