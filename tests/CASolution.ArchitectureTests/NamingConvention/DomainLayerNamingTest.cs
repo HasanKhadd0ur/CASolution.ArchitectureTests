0efde873f0da373919e836c866f6e2c8731dde6f
hasan khaddour <hasan.b.khaddour@gmail.com>
2024-09-27 19:23:22 +0300

    Add Domain Layer Naming Tests

--- tests/CASolution.ArchitectureTests/NamingConvention/DomainLayerNamingTest.cs
+++ tests/CASolution.ArchitectureTests/NamingConvention/DomainLayerNamingTest.cs
@@ -1,4 +1,9 @@
 ﻿using CASolution.ArchitectureTests.Providers;
+using CASolution.SharedKernel.DomainEvents;
+using CASolution.SharedKernel.Repositories;
+using FluentAssertions;
+using NetArchTest.Rules;
+using Xunit;
 
 namespace CASolution.ArchitectureTests.NamingConvention
 {
@@ -7,6 +12,39 @@ namespace CASolution.ArchitectureTests.NamingConvention
         public DomainLayerNamingTest(IAssembliesProvider assembliesProvider) : base(assembliesProvider)
         {
         }
+
+        #region Events Naming Convention 
+        [Fact]
+        public void Event_ShouldHave_NameEndingWith_Event()
+        {
+           Helpers
+                .NamingConventionHelper
+                .InterfaceImplementingTest(
+                    AssembliesProvider.DomainAssembly,
+                    typeof(IDomainEvent),
+                    "Event"
+                    ).Should().BeTrue();
+        }
+
+        #endregion Events Naming Convention
+
+
+        #region Repository Naming Convention 
+        [Fact]
+        public void Repositories_ShouldHave_NameEndingWith_Repository()
+        {
+            Helpers
+             .NamingConventionHelper
+             .InterfaceImplementingTest(
+                 AssembliesProvider.DomainAssembly,
+                 typeof(IRepository<>),
+                 "Repository"
+                 ).Should().BeTrue();
+
+        }
+
+        #endregion Repository Naming Convention
+
     }
 
 }
